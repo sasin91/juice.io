3313ab51f607e9072f33ed3ce82908220046ac95
Jonas Kervin Hansen <jonas.kerwin.hansen@gmail.com>
2022-11-03 11:10:27 +0100

    added tests to cover auth

---
+++ tests/Controllers/LoginControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Tests\Controllers;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use function random_bytes;
+
+class LoginControllerTest extends WebTestCase
+{
+    public function testCanLogin(): void
+    {
+        $client = static::createClient();
+        $client->followRedirects();
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+        $user = new User();
+        $user->setEmail(sha1(random_bytes(5)).'@example.com');
+        $user->setPassword(
+            static::getContainer()->get(UserPasswordHasherInterface::class)->hashPassword(
+                $user,
+                'password'
+            )
+        );
+
+        $userRepository->save($user, true);
+
+        $crawler = $client->request('POST', '/login', [
+            '_username' => $user->getEmail(),
+            '_password' => 'password'
+        ]);
+
+        $this->assertResponseIsSuccessful();
+    }
+}
---
+++ tests/Controllers/RegistrationControllerTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Tests\Controllers;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RegistrationControllerTest extends WebTestCase
+{
+    public function testCanRegisterANewUser()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request(
+            method:'POST',
+            uri: '/register',
+            parameters: [
+                'registration_form[email]' => 'john@example.com',
+                'registration_form[plainPassword]' => 'password',
+                'registration_form[agreeTerms]' => 'yes'
+            ]
+        );
+
+        $this->assertResponseIsSuccessful();
+    }
+}

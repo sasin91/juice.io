3a770eeab63cbaba8961e74bb26bb52cd729010f
jkh <jkh@syncronet.dk>
2022-11-22 10:36:21 +0100

    use .env.local rather than shared .env

--- bin/setup.sh
+++ bin/setup.sh
@@ -22,5 +22,14 @@ then
   fi
 fi
 
+if [ ! -f ".env.local" ]; then
+  echo "Could not find local env, creating."
+  read -r -p "Whats your DB Username? " DB_USERNAME
+  read -r -p "Whats your DB Password? [$DB_USERNAME] " DB_PASSWORD
+  read -r -p "Whats your DB name? [juice] " DB_DATABASE
+
+  echo "DATABASE_URL=\"postgresql://$DB_USERNAME:${DB_PASSWORD:-$DB_USERNAME}@127.0.0.1:5432/${DB_DATABASE:-juice}?serverVersion=14&charset=utf8\"" >> .env.local
+fi
+
 symfony console doctrine:database:create
 symfony console doctrine:migrations:migrate

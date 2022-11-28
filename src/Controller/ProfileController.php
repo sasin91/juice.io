8e2052b167d33590879c121dd53411ce3e50f23e
jkh <jkh@syncronet.dk>
2022-11-28 13:58:29 +0100

    Added Profile path

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProfileController extends AbstractController
+{
+    #[Route(
+        path: [
+            'en' => '/profile',
+            'da' => '/profil'
+        ],
+        name: 'app_profile',
+        methods: ['OPTIONS','GET']
+    )]
+    public function index(): Response
+    {
+        // usually you'll want to make sure the user is authenticated first,
+        // see "Authorization" below
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        // returns your User object, or null if the user is not authenticated
+        // use inline documentation to tell your editor your exact User class
+        /** @var User $user */
+        $user = $this->getUser();
+
+        // Call whatever methods you've added to your User class
+        // For example, if you added a getFirstName() method, you can use that.
+        return $this->render('profile/index.html.twig', [
+            'user' => $user
+        ]);
+    }
+}

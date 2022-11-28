6692d030c0142e69c12d1f1f05e153f7df4be31f
jkh <jkh@syncronet.dk>
2022-11-28 13:21:15 +0100

    Transition effect on desktop auth menu

--- assets/controllers/auth_menu_controller.js
+++ assets/controllers/auth_menu_controller.js
@@ -1,4 +1,5 @@
 import {Controller} from '@hotwired/stimulus';
+import { useTransition } from 'stimulus-use';
 
 /*
 * The following line makes this controller "lazy": it won't be downloaded until needed
@@ -8,7 +9,24 @@ import {Controller} from '@hotwired/stimulus';
 export default class extends Controller {
     static targets = ['menu']
 
+    connect() {
+        this.menuTarget.hidden = true
+
+        useTransition(this, {
+            element: this.menuTarget,
+            enterActive: 'fade-enter-active',
+            enterFrom: 'fade-enter-from',
+            enterTo: 'fade-enter-to',
+            leaveActive: 'fade-leave-active',
+            leaveFrom: 'fade-leave-from',
+            leaveTo: 'fade-leave-to',
+            hiddenClass: 'hidden',
+            transitioned: !this.menuTarget.hidden,
+        });
+    }
+
     toggleMenu() {
         this.menuTarget.hidden = !this.menuTarget.hidden
+        this.toggleTransition();
     }
 }
